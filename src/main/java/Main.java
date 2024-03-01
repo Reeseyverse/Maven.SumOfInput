a81fc20c7927ae10a7f81c403ac1facdf5a1adda
Reeseyverse <jorrisecko@gmail.com>
2024-03-01 15:33:36 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,40 +2,19 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
  */
-public class Main {
 
-    public static void main(String[] args){
-        
-        int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
 
-    }
-    
-    // do this one and the lab is complete. really.
-    // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
-    }
-    
-    
-    int getInputNumber() { // gee, may thi sis useful in another lab?
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter any number: ");
+        public class Main {
+            public static void main(String[] args) {
 
-        // This method reads the number provided using keyboard
-        int num = scan.nextInt();
+                int number = Num.numTotal(3);
+                System.out.println(number);
+            }
 
-        // Closing Scanner after the use
-        scan.close();
-        return num;
-    }
-    
-    
-    
-    // for Extra Credit
+
+
+
+            // for Extra Credit
     int gaussianSumOfNumbers(int n) {
         return 0;
     }
---
+++ src/main/java/Num.java
@@ -0,0 +1,10 @@
+public class Num {
+
+    public static int numTotal(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
